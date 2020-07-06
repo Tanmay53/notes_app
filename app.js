896c5273e8e7937c9b58815057fec8bd5c6c4161
Tanmay53 <tanmaykhatri53@gmail.com>
2020-07-07 01:14:52 +0530

    Added yargs and learnt command line parsing through it.

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-var msg = getNotes();
-console.log(msg);
+// Create Remove Command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log(chalk.green("Adding a new note."));
+  }
+})
 
-console.log(chalk.bgWhite.bold.italic.rgb(20, 129, 30)(" Success "));
+// Creating a Remove Command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note.",
+  handler: function() {
+    console.log(chalk.red("Removing a note."));
+  }
+})
 
-console.log(process.argv);
+// Creating a list Command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function() {
+    console.log(chalk.yellow("Listing all the notes."));
+  }
+})
+
+// Creating a read Command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log(chalk.blue("Reading a note."));
+  }
+})
+
+console.log(yargs.argv);
