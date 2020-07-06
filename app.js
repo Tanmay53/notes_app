83cbe2201362a6b147710882fcf3e2c98b1528dc
Tanmay53 <tanmaykhatri53@gmail.com>
2020-07-06 23:45:57 +0530

    Chalk and Validator Node Modules Added.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 var msg = getNotes();
-
 console.log(msg);
+
+console.log(chalk.bgWhite.bold.italic.rgb(20, 129, 30)(" Success "));
+
+console.log(process.argv);
